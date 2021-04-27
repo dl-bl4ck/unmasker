eaa0237a91add9e1d127d81b9982e6cb3f04c74e
Paras Mehan <parasmehan123@gmail.com>
2021-04-27 16:37:28 +0530

    dataset changed

--- dataset.py
+++ dataset.py
@@ -12,26 +12,24 @@ class masked_dataset(Dataset):
         self.masked = []
         
         for i in tqdm((Path(path)/'normal').glob("*.jpg")):
-            img = Image.open(i).resize((224,224))
-            x = np.array(img) 
-
+            self.normal.append(i)
             name = str(i).split('/')[-1]
-            x = transforms.ToTensor()(img)
-            self.normal.append(x)     
-
-            mask = Image.open(Path(path)/f"masked/{name}").resize((224,224))
-            mask = np.array(mask) 
-            self.masked.append(transforms.ToTensor()(mask))
-
+            self.masked.append(Path(path)/f"masked/{name}")
+            
             if len(self.masked) >= maxx:
                 break
 
-        self.normal = torch.stack(self.normal ,dim = 0)
-        self.masked = torch.stack(self.masked ,dim = 0)
-        #print(self.normal.shape,self.masked.shape)
             
     def __getitem__(self, idx):
-        return self.normal[idx],self.masked[idx]
+        img = Image.open(self.normal[idx]).resize((224,224))
+        x = np.array(img) 
+        x = transforms.ToTensor()(img)
+ 
+        mask = Image.open(self.masked[idx]).resize((224,224))
+        mask = np.array(mask) 
+        mask = transforms.ToTensor()(mask)
+
+        return x,mask
 
     def __len__(self):
         return len(self.masked)
